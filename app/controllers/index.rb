ce662056291f04f2a540d8a67f2b48c4e2bdf84a
alookatommorow <hessjohnt@gmail.com>
2015-11-01 21:53:37 -0800

    add dashboard post route and error handling for invalid org or repo

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,3 +8,10 @@ get '/dashboard' do
   @contributors = Github::Client.new.contributors
   erb :dashboard
 end
+
+post '/dashboard' do
+  @org_name = params[:organization].downcase
+  @repo_name = params[:repo].downcase
+  @contributors = Github::Client.new.contributors(@org_name, @repo_name)
+  erb :dashboard
+end
